fa903530f9fba29ab79ab421837a314aec7d738c
Cook <dtc2@NIST.GOV>
2021-10-26 10:58:54 -0700

    set up eq pga driver to run through model set

--- driver_hazus_eq_pga.m
+++ driver_hazus_eq_pga.m
@@ -3,26 +3,27 @@ close all
 clc
 rehash
 
-%% Define Building and Site Inputs
-% Site info
-site = struct('lat', 39.0618, 'lng', -77.0536);  % LA
-
-% Building
-haz_building_class = 'C1';
-hazus_zone = '4';
-num_stories = 4;
-year_of_construction = 2020;
-hazus.occupancy = 'COM4';
-
 %% Import Packages
 import hazus.*
 
-%% Pull Shakemap data for this site
-[ pga ] = fn_read_shakemap_data( site.lat, site.lng, 'mineral_VA' );
+%% Load model data
+models = readtable(['inputs' filesep 'models.csv']);
 
-%% Define transitionary Inputs
-[ hazus.building_type ] = fn_hazus_building_type( haz_building_class, num_stories );
-[ hazus.code_level ] = fn_hazus_code_level( hazus_zone, hazus.building_type, year_of_construction );
+%% Run each model through hazus eq pga
+for m = 1:height(models)
+    %% Pull Shakemap data for this site
+    [ models.pga(m) ] = fn_read_shakemap_data( models.lat(m), models.lng(m), 'mineral_VA' );
 
-%% Run Hazus
-[ loss ] = main_hazus_eq_pga( hazus.building_type.build_type, hazus.occupancy, hazus.code_level, pga );
+    %% Define transitionary Inputs
+    [ building_type ] = fn_hazus_building_type( models.building_type_id{m}, models.num_stories(m) );
+    [ models.code_level{m} ] = fn_hazus_code_level( num2str(models.hazus_zone(m)), building_type, models.year_of_construction(m) );
+
+    %% Run Hazus
+    [ models.loss(m) ] = main_hazus_eq_pga( building_type.build_type, models.occupancy{m}, models.code_level(m), models.pga(m) );
+end
+
+%% Save output data
+if ~exist('outputs','dir')
+    mkdir('outputs')
+end
+writetable(models, ['outputs' filesep 'model_outputs.csv'])
