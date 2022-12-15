bfd539beb97250e31c226da224e9645750af07ca
Cook <dtc2@NIST.GOV>
2022-12-15 13:38:01 -0500

    refine pgd assessment for tracks and tunnel
    consider probability of ground failure and combine assessment of various
    asset types into one main function

---
+++ +hazus/main_hazus_rail.m
@@ -0,0 +1,80 @@
+function [ loss ] = main_hazus_rail( comp, n_sims )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Hazus Notes
+% The reference spectrum represents ground
+% shaking of a large-magnitude (i.e., M ? 7.0) western United States (WUS) earthquake for soil
+% sites (e.g., Site Class D) at site-to-source distances of 15 km, or greater
+% uncertainty in the damage?state threshold of the structural system Beta_M(SPGA) = 0.4 for all building types and damage states),
+% variability in response due to the spatial variability of ground motion demand B_D(V) = 0.5 for long?period spectral response).
+
+%% Initial Setup
+% Import packages
+import hazus.sim_hazus_ds
+import hazus.fn_hazus_loss
+
+% Load and filter eq pga data
+if strcmp(comp.lifeline_type,'RST') % only for stations use equivalent pga data
+    eq_pga_data = readtable(['+hazus' filesep 'data_hazus' filesep 'hazus_eq_pga_datatable.csv']);
+    filt = strcmp(eq_pga_data.loc,comp.code_level) & strcmp(eq_pga_data.build_type,comp.build_type);
+    fragility_data_eq_pga = eq_pga_data(filt,:);
+end
+
+% Load and filter fragility data
+fragility_table = readtable(['+hazus' filesep 'data_hazus' filesep 'hazus_rail_fragility.csv']);
+fragility_data = fragility_table(strcmp(fragility_table.lifeline_class,comp.lifeline_class),:);
+
+% Load and filter consequence data
+consequence_table = readtable(['+hazus' filesep 'data_hazus' filesep 'hazus_rail_consequence.csv']);
+consequence_data = consequence_table(strcmp(consequence_table.lifeline_type,comp.lifeline_type),:);
+
+%% Calculate Building Damage based on ground shaking
+if strcmp(comp.lifeline_type,'RST') % only for stations use equivalent pga data
+    [ ds_pga ] = sim_hazus_ds( fragility_data_eq_pga, comp.pga, n_sims );
+elseif any(ismember(fragility_data.intensity_measure,'pga')) % for other rail network components sensitive to pga
+    fragility_data_pga = fragility_data(strcmp(fragility_data.intensity_measure,'pga'),:);
+    [ ds_pga ] = sim_hazus_ds( fragility_data_pga, comp.pga, n_sims );
+else
+    ds_pga = false(n_sims,4);
+end
+
+%% Calculate Building Damage based on ground failure
+% Simulate ground failure occurances
+sim_liq = rand(n_sims, 1) < comp.p_liq; % Simulate which realizations have liquefaction
+sim_land = rand(n_sims, 1) < comp.p_land; % Simulate which realizations have landslide
+sim_rupt = rand(n_sims, 1); % Simulate extent of surface fault rupture deformations
+
+% Damage from lateral spreading
+filt = strcmp(fragility_data.hazard,'lateral spread') | strcmp(fragility_data.hazard,'ground failure');
+fragility_data_filt = fragility_data(filt,:);
+[ ds_lat_spread ] = sim_hazus_ds( fragility_data_filt, comp.pgd_lat, n_sims );
+
+% Damage from vertical settlement
+filt = strcmp(fragility_data.hazard,'vertical settlement') | strcmp(fragility_data.hazard,'ground failure');
+fragility_data_filt = fragility_data(filt,:);
+[ ds_settlement ] = sim_hazus_ds( fragility_data_filt, comp.pgd_set, n_sims );
+
+% Damage from fault rupture
+filt = strcmp(fragility_data.hazard,'fault rupture') | strcmp(fragility_data.hazard,'ground failure');
+fragility_data_filt = fragility_data(filt,:);
+pgd_rupt = comp.pgd_rup .* sim_rupt;
+[ ds_rupture ] = sim_hazus_ds( fragility_data_filt, pgd_rupt, n_sims );
+
+% Damage from landslide
+filt = strcmp(fragility_data.hazard,'landslide') | strcmp(fragility_data.hazard,'ground failure');
+fragility_data_filt = fragility_data(filt,:);
+[ ds_landslide ] = sim_hazus_ds( fragility_data_filt, comp.pgd_lan, n_sims );
+
+%% Combine damage state occurances
+ds_sim_total = ds_pga | ds_lat_spread.*sim_liq | ds_settlement.*sim_liq | ds_rupture | ds_landslide.*sim_land;
+prob_ds_exceed = mean(ds_sim_total);
+
+% damage state probabilities
+prob_ds = prob_ds_exceed - [prob_ds_exceed(2:end),0];
+
+%% Calculate Building Loss
+[ loss ] = fn_hazus_loss( prob_ds, consequence_data, 'mean_recovery_days' );
+
+end
+
--- driver_hazus_railnetwork.m
+++ driver_hazus_railnetwork.m
@@ -3,34 +3,42 @@ close all
 clc
 rehash
 
-%% Import Packages
+
+%% Intial setup
+% Import Packages
 import hazus.*
 
-%% Load model data
+% Load model data
 network_components = readtable(['inputs' filesep 'network_components.csv']);
+
+% Define analysis settings
+n_sims = 10000;
 
 %% Run each model through hazus eq pga
 for m = 1:height(network_components)
-    %% Pull Shakemap data for this site
+    % Pull Shakemap data for this site
     network_components.pga(m) = 0.5; % Just fix to 0.5 for now
-    network_components.pgd(m) = network_components.pga(m) * 8; % estimate PGD based on very simple and very incorrect approximation
+    network_components.pgd_lat(m) = network_components.pga(m) * 8; % estimate PGD based on very simple and very incorrect approximation
+    network_components.pgd_set(m) = network_components.pga(m) * 8; % estimate PGD based on very simple and very incorrect approximation
+    network_components.pgd_lan(m) = network_components.pga(m) * 8; % estimate PGD based on very simple and very incorrect approximation
+    network_components.pgd_rup(m) = network_components.pga(m) * 8; % estimate PGD based on very simple and very incorrect approximation
+    network_components.p_liq(m) = 0.1;  % Assume 10% prob of liquefaction associated with moderate susceptability
+    network_components.p_land(m) = 0.08; % Assume 8% prob of landslide associated with moderate susceptability
 %     [ network_components.pga(m) ] = fn_read_shakemap_data( network_components.lat(m), network_components.lng(m), 'mineral_VA' );
 
-    %% Calc losses for each asset
-    if strcmp(network_components.lifeline_type(m),'RTR')
-        % Determine Hazus Loss
-        [ network_components.recovery_time_days(m) ] = main_hazus_rail_track( network_components.pgd(m) );
-    elseif strcmp(network_components.lifeline_type(m),'RST')
+    % Define attributes and shake data fort this component
+    comp = table2struct(network_components(m,:));
+    
+    % Pull Hazus building data for stations
+    if strcmp(network_components.lifeline_type(m),'RST')
         % Define transitionary Inputs
         [ building_type ] = fn_hazus_building_type( network_components.building_type_id{m}, network_components.num_stories(m) );
-        [ network_components.code_level{m} ] = fn_hazus_code_level( num2str(network_components.hazus_zone(m)), building_type, network_components.year_of_construction(m) );
-
-        % Run Hazus Eq PGA method and PGD for stations
-        [ network_components.recovery_time_days(m) ] = main_hazus_rail_station( building_type.build_type, network_components.code_level(m), network_components.pga(m), network_components.pgd(m) );
-    elseif strcmp(network_components.lifeline_type(m),'RTU')
-        % Determine Hazus losses for tunnels
-        [ network_components.recovery_time_days(m) ] = main_hazus_rail_tunnel( network_components.pga(m), network_components.pgd(m) );
+        comp.build_type = building_type.build_type;
+        [ comp.code_level ] = fn_hazus_code_level( num2str(network_components.hazus_zone(m)), building_type, network_components.year_of_construction(m) );
     end
+
+    % Calc losses for each asset
+    [ network_components.recovery_time_days(m) ] = main_hazus_rail( comp, n_sims );
 end
 
 %% Save output data
